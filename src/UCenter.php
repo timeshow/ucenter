57289d7ba26b04616ecd01a523599003a4756c22
timeshow <397975896@qq.com>
2020-05-12 10:17:10 +0800

    添加uc_client客户端

---
+++ src/UCenter.php
@@ -0,0 +1,43 @@
+<?php
+namespace TimeShow\UCenter;
+
+use Config;
+use mysql_xdevapi\Exception;
+
+class UCenter
+{
+    public function __construct()
+    {
+        if(!defined('UC_DBHOST')){
+            $config = Config::get('ucenter');
+            define('UC_CONNECT',$config['connect']);
+            define('UC_DBHOST',$config['dbhost']);
+            define('UC_DBUSER',$config['dbuser']);
+            define('UC_DBPW',$config['dbpw']);
+            define('UC_DBNAME',$config['dbname']);
+            define('UC_DBCONNECT',$config['dbconnect']);
+            define('UC_DBCHARSET',$config['dbcharset']);
+            define('UC_DBTABLEPRE',$config['dbtablepre']);
+            if (!defined('UC_KEY')) {
+                define('UC_KEY', $config['key']);
+            }
+            define('UC_API',$config['api']);
+            define('UC_CHARSET',$config['charset']);
+            define('UC_IP',$config['ip']);
+            define('UC_APPID',$config['appid']);
+            define('UC_PPP',$config['ppp']);
+            include __DIR__.'uc_client/client.php';
+        }
+    }
+
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement __call() method.
+        if(function_exists($name)){
+            return call_user_func_array($name,$arguments);
+        }else{
+            throw new Exception('function name not exists');
+        }
+    }
+
+}
--- src/UCenterServiceProvider.php
+++ src/UCenterServiceProvider.php
@@ -14,6 +14,11 @@ class UCenterServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        include __DIR__.'routes.php';
+        $this->publishes([
+            __DIR__.'/config/ucenter.php'=>config_path('ucenter.php'),
+        ]);
+        $this->mergeConfigForm(__DIR__.'config/ucenter.php','ucenter');
     }
 
     /**
@@ -24,5 +29,9 @@ class UCenterServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind('ucenter',function ($app){
+            return new UCenter;
+        });
+        $this->app->bind('TimeShow\UCenter\Contracts\Api',config('ucenter.service'));
     }
 }
